2cebf3145d34ad342a2394cf5a984288d8dd7cce
AJ <amanda.elisabet.jordan@gmail.com>
2019-03-24 16:49:30 -0400

    timeline layout is working!!

---
+++ client/src/Components/TimelineEvent/TimelineEvent.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import VisibilitySensor from 'react-visibility-sensor';
+
+class TimelineEvent extends Component {
+  constructor(props) {
+    super(props);
+    this.onVisibilitySensorChange = this.onVisibilitySensorChange.bind(this);
+    this.state = { visible: false };
+  }
+
+  onVisibilitySensorChange(isVisible) {
+    if (isVisible) {
+      this.setState({ visible: true });
+    }
+  }
+
+  render() {
+    const {
+      id,
+      children,
+      icon,
+      iconStyle,
+      iconOnClick,
+      primaryDate,
+      position,
+      style,
+      className,
+      visibilitySensorProps,
+      type,
+      name,
+      director,
+      location,
+      concertSeason,
+      description,
+      imageURL
+    } = this.props;
+
+    const { visible } = this.state;
+
+    return (
+      <div
+        id={id}
+        className={classNames(className, 'vertical-timeline-element', {
+          'vertical-timeline-element--left': position === 'left',
+          'vertical-timeline-element--right': position === 'right',
+          'vertical-timeline-element--no-children': children === ''
+        })}
+        style={style}
+      >
+        <VisibilitySensor
+          {...visibilitySensorProps}
+          onChange={this.onVisibilitySensorChange}
+        >
+          <div>
+            <span // eslint-disable-line jsx-a11y/no-static-element-interactions
+              style={iconStyle}
+              onClick={iconOnClick}
+              className={`vertical-timeline-element-icon ${
+                visible ? 'bounce-in' : 'is-hidden'
+              }`}
+            >
+              {icon}
+            </span>
+
+            <div
+              className={`vertical-timeline-element-content ${
+                visible ? 'bounce-in' : 'is-hidden'
+              }`}
+            >
+              <div className='imageBlock'>
+                <img
+                  className='responsive eventImg'
+                  src={imageURL}
+                  alt={name}
+                />
+              </div>
+              <h3 className='vertical-timeline-element-title'>{name}</h3>
+              <h4 className='vertical-timeline-element-subtitle'>
+                Directed by {director}
+              </h4>
+              <p>{description}</p>
+              <p>
+                {type} {location} {concertSeason}
+              </p>
+              <span className='vertical-timeline-element-primaryDate'>
+                {primaryDate}
+              </span>
+            </div>
+          </div>
+        </VisibilitySensor>
+      </div>
+    );
+  }
+}
+
+TimelineEvent.propTypes = {
+  id: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  className: PropTypes.string,
+  icon: PropTypes.element,
+  iconStyle: PropTypes.shape({}),
+  iconOnClick: PropTypes.func,
+  style: PropTypes.shape({}),
+  primaryDate: PropTypes.node,
+  position: PropTypes.string,
+  visibilitySensorProps: PropTypes.shape({}),
+  type: PropTypes.string,
+  name: PropTypes.string,
+  director: PropTypes.string,
+  location: PropTypes.string,
+  concertSeason: PropTypes.string,
+  description: PropTypes.string,
+  imageURL: PropTypes.string
+};
+
+TimelineEvent.defaultProps = {
+  id: '',
+  children: '',
+  className: '',
+  icon: null,
+  iconStyle: null,
+  style: null,
+  primaryDate: '',
+  position: '',
+  iconOnClick: null,
+  visibilitySensorProps: { partialVisibility: true, offset: { bottom: 80 } },
+  type: '',
+  name: '',
+  director: '',
+  location: '',
+  concertSeason: null,
+  description: '',
+  imageURL:
+    'https://carepharmaceuticals.com.au/wp-content/uploads/sites/19/2018/02/placeholder-600x400.png'
+};
+
+export default TimelineEvent;
--- client/src/Pages/MainTimeline.js
+++ client/src/Pages/MainTimeline.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import API from './../utils/API';
+import VerticalTimeline from './../Components/VerticalTimeline/VerticalTimeline';
+import TimelineEvent from './../Components/TimelineEvent/TimelineEvent';
+import NewReleases from '@material-ui/icons/NewReleases';
 
 class MainTimeline extends Component {
   constructor(props) {
@@ -20,20 +23,28 @@ class MainTimeline extends Component {
   render() {
     return (
       <div className='MainContainer'>
-        <div className='pure-g'>
-          <div className='pure-u-1-3' />
-          <div className='pure-u-1-3'>
-            <h3>AGMC History</h3>
-          </div>
-          <div className='pure-u-1-3' />
-        </div>
-        <div className='pure-g'>
-          <div className='pure-11-24' />
-          <div className='pure-1-12' id='middle'>
-            ||||
-          </div>
-          <div className='pure-11-24' />
-        </div>
+        <h3>AGMC History</h3>
+
+        <VerticalTimeline layout='2-columns'>
+          <TimelineEvent
+            primaryDate='1981'
+            icon={<NewReleases />}
+            name='AGMC Founded'
+            type='Special Event'
+            description={
+              "In 1981, the fact that a group of gay men in Atlanta formed a chorus and actually had the temerity to call itself the Atlanta GAY MEN'S Chorus was a bold and courageous act--and we must not forget the bravery our first members displayed simply by BEING a member. From the very start, Atlanta's gay and lesbian community embraced the AGMC--its very existence a proud statement of self-empowerment. Thirty-eight years later, membership in the Atlanta Gay Men's Chorus for many members is still a huge step towards affirmation and validation of their identity as gay men."
+            }
+            location='Atlanta, GA'
+          />
+          <TimelineEvent
+            primaryDate='October 18, 1981'
+            name='Sneak Preview'
+            location='(First) Metropolitan Community Church Virginia Highlands'
+            icon={<NewReleases />}
+            concertSeason='First Concert Season'
+            director='Jeffrey D. McIntyre'
+          />
+        </VerticalTimeline>
       </div>
     );
   }
